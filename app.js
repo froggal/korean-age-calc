b01e60f5d36e6baea63b31d3d4188912d36794ca
froggal <doeon111@naver.com>
2023-04-22 21:40:45 +0900

    done

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 function submitBirth(){
-    const birthDay = document.querySelector("birthday").value
+    const birthDay = document.querySelector("#birthday").value
     var y = birthDay.split('-')[0]
     var m = birthDay.split('-')[1]
     var d = birthDay.split('-')[2]
-    loadToday()
-}
-
-function loadToday(){
     const today = new Date()
     var ty = today.getFullYear()
     var tm = today.getMonth()
     var td = today.getDate()
-    convert()
-}
-
-function convert(){
+    
     age = ty - y
     if(m == tm){
         if(d == td){
             age = age + 1
         }
     }
+
+    document.getElementById('converted').innerHTML = "만 " + age + " 세"
 }
