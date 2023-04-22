7938d665fde16485c7b94bba25a084e6ab48bd87
froggal <doeon111@naver.com>
2023-04-22 21:22:33 +0900

    create load today code

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function submitBirth(){
-    const birthDay = document.querySelector("birthday").value;
+    const birthDay = document.querySelector("birthday").value
     var y = birthDay.split('-')[0]
     var m = birthDay.split('-')[1]
     var d = birthDay.split('-')[2]
+    loadToday()
+}
+
+function loadToday(){
+    const today = new Date()
+    var year = today.getFullYear()
+    var month = today.getMonth()
+    var day = today.getDate()
 }
