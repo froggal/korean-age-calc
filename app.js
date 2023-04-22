1628c0a69ad1a52ccc050b8f39e569cdc389dada
froggal <doeon111@naver.com>
2023-04-22 21:20:04 +0900

    inputbox date split

---
+++ app.js
@@ -0,0 +1,6 @@
+function submitBirth(){
+    const birthDay = document.querySelector("birthday").value;
+    var y = birthDay.split('-')[0]
+    var m = birthDay.split('-')[1]
+    var d = birthDay.split('-')[2]
+}
