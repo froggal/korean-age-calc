ca5d45d8e621596168ddf602f87c645cf40439b3
froggal <doeon111@naver.com>
2023-04-23 19:36:18 +0900

    bug fixed, finally!

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function submitBirth(){
     const birthDay = document.querySelector("#birthday").value
-    var y = birthDay.split('-')[0]
-    var m = birthDay.split('-')[1]
-    var d = birthDay.split('-')[2]
+    var y = Number(birthDay.split('-')[0])
+    var m = Number(birthDay.split('-')[1])
+    var d = Number(birthDay.split('-')[2])
     const today = new Date()
     var ty = today.getFullYear()
-    var tm = today.getMonth()
+    var tm = today.getMonth() + 1
     var td = today.getDate()
     
     age = ty - y // 2023 - 2011 = 12
-    if(m > tm){
+    if(m > tm || (m == tm && d > td)) { // 4 > 4 = false
         age = age - 1
     }
 
